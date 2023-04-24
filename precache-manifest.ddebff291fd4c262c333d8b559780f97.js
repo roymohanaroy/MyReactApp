5e822ce08fe7b75018e5ebd0fed89e7547d126ad
mohana <mohana.roy@gmail.com>
2023-04-24 23:00:40 +0800

    Updates

---
+++ precache-manifest.ddebff291fd4c262c333d8b559780f97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c46433b2b24058cb38f0",
+    "url": "%7Broymohanaroy%7D.github.io/static/css/main.683a1986.chunk.css"
+  },
+  {
+    "revision": "c46433b2b24058cb38f0",
+    "url": "%7Broymohanaroy%7D.github.io/static/js/main.1ff403c7.chunk.js"
+  },
+  {
+    "revision": "e8b012c3338807259b76",
+    "url": "%7Broymohanaroy%7D.github.io/static/js/runtime~main.321f4e97.js"
+  },
+  {
+    "revision": "d81e486966e498134770",
+    "url": "%7Broymohanaroy%7D.github.io/static/js/2.825c2a74.chunk.js"
+  },
+  {
+    "revision": "c601944039ff5f8917f93f55c28b8606",
+    "url": "%7Broymohanaroy%7D.github.io/index.html"
+  }
+];
